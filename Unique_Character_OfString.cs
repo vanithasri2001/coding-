113ee61f27da6aef19fa44b49dcabd8b877ce15c
vanithasri2001 <115348508+vanithasri2001@users.noreply.github.com>
2023-11-28 10:24:54 +0530

    Add files via upload

---
+++ Unique_Character_OfString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    class Unique_Character_OfString
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter the string");
+            input = Console.ReadLine();
+            char uniqueChar = '\0';
+            foreach (char c in input)
+            {
+                if (input.IndexOf(c) == input.LastIndexOf(c))
+                {
+                    
+                    uniqueChar = c;
+                    
+                    break;
+                }
+            }
+            if (uniqueChar == '\0')
+            {
+                Console.WriteLine("No unique character found");
+            }
+            else
+            {
+                Console.WriteLine("The unique character is:" + uniqueChar);
+            }
+        }
+    }
+}
+
+
+
+
